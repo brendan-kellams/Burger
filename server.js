64424ee26dc93aef57bdb4d37555e2df6d46ce23
Brendan Kellams <brendan.kellams@gmail.com>
2017-12-08 22:17:40 -0800

    made changes to server, controller, and handlebars

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 'use strict';
 
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var expresshandlebars = require('express-handlebars');
+
+var app = express();
+app.use(express.static(process.cwd() + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(methodOverride("_method"));
+app.engine('handlebars', expresshandlebars({
+    defaultLayout: 'main'
+}));
+app.set('view-engine', 'handlebars');
+
+var PORT = process.env.PORT || 3000;
+app.listen(PORT);
 
 var connection = require('./config/connection');
 
@@ -18,9 +35,3 @@ connection.query('SELECT * FROM burgers', function (err, data) {
 //     console.log("Data from the orm!!");
 //     console.log(data);
 // })
-
-// var PORT = process.env.PORT || 3000;
-
-// var app = express();
-
-// app.use(express.static(process.cwd() + '/public'));
