d183e4f5f7932d29770f20540c790eca7e27ed40
Brendan Kellams <brendan.kellams@gmail.com>
2018-02-15 15:23:24 -0800

    changing things with JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,17 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: 'root',
+//     database: "burgers_db"
+// });
 
-connection.connect(function(err) {
-    if(err)throw err;
-    console.log("connected as id: " + connection.threadId);
-});
+// connection.connect(function(err) {
+//     if(err)throw err;
+//     console.log("connected as id: " + connection.threadId);
+// });
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
